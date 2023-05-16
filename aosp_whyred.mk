4264e9329ef5630e0a41cf2dfc52da1546bb92a8
RomBuilder-Segfault <132055572+NopeNopeGuy@users.noreply.github.com>
2023-05-16 14:40:02 +0530

    Fix Common

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # PixelExperience Properties
 TARGET_INCLUDE_WIFI_EXT := true
